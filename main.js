6a66f92f162ec2d2bbf20d0777f687fb740dd8f6
Sam Hadfield <sthtech@protonmail.com>
2025-09-09 18:08:18 -0500

    Clean up testing environment

--- main.js
+++ main.js
@@ -2,16 +2,46 @@
 
 import { HashMap } from "./hashmap.js";
 
-const hashMap = new HashMap();
+const test = new HashMap();
 
-// console.log(hashMap.hash("Rama"));
-// console.log(hashMap.hash("Sita"));
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
 
-hashMap.set("Rama", "This hash is for Rama");
-hashMap.set("Sita", "This new hash is for Sita");
-hashMap.set("maRa", "This new hash is for maRa");
+// 'Get' Method
+// console.log(test.get("apple"));
 
-// console.log(hashMap.hash("maRa"));
-// hashMap.toString();
+// 'Has' Method
+// console.log(test.has("ice cream"));
 
-// console.log(hashMap.bucket);
+// 'Remove' Method
+// console.log(test.remove("jacket"));
+
+// 'Length' Method
+// console.log(test.length());
+
+// 'Clear' Method
+// test.clear();
+
+// 'Keys' Method
+// console.log(test.keys());
+
+// 'Values' Method
+// console.log(test.values());
+
+// 'Entries' Method
+// console.log(test.entries());
+
+// Test Capacity
+// test.set("moon", "silver");
+
+console.log(test.buckets.length);
